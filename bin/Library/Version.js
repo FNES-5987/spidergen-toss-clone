321c53337726656a965e934da5eb9f3d60180173
JJB <137266742+Jelly-Fish-J@users.noreply.github.com>
2024-02-02 14:24:13 +0900

    ~section7 lay

---
+++ bin/Library/Version.js
@@ -0,0 +1,22 @@
+
+	var _version = {};
+
+	_version.setFileVersion = function(){
+			afc.setVersionMap({
+				 'Assets/image/applekorea.png':1706847279192
+				,'Assets/image/googleplay.png':1706847279192
+				,'Assets/image/iPhone12_Clay_Shadow.png':1706847279192
+				,'Assets/image/icon-alarm-3.png':1706847279192
+				,'Assets/image/icon-bulb-2.png':1706847279192
+				,'Assets/image/icon-credit-grade-check-2.png':1706847279192
+				,'Assets/image/icon-credit-grade-up-2.png':1706847279192
+				,'Assets/image/new_main.png':1706847279192
+				,'Assets/image/section1_1_home_01.png':1706847279192
+				,'Assets/image/section1_1_home_02.png':1706847279192
+				,'Assets/image/section1_2_01.png':1706847279192
+				,'Assets/image/section1_2_02.png':1706847279192
+				,'Assets/image/section1_2_03.png':1706847279192
+				,'Assets/image/toss_logo.jpg':1706847279192
+			});
+	};
+
