24eeb7d7b4a79fdc1192fd8b24baf0159c4bccf9
JJB <137266742+Jelly-Fish-J@users.noreply.github.com>
2024-02-05 14:36:26 +0900

    section4제외 완성

--- bin/Source/MainView.js
+++ bin/Source/MainView.js
@@ -20,21 +20,41 @@ MainView.prototype.init = function(context, evtListener)
 	var thisObj = this;
 
 	window.addEventListener('scroll', function() {
-		console.log(window.scrollY);
-		/*if (window.scrollY >= 1000) {
-			thisObj.scrollInSection2();
-		} else if (window.scrollY >= 2600) {
-			thisObj.scrollInSection3();
-		}*/
-		window.scrollY >= 1000 ? thisObj.scrollInSection_rise(2) : null;
-		window.scrollY >= 2600 ? thisObj.scrollInSection_rise(3) : null;
-		window.scrollY >= 5000 ? thisObj.scrollInSection_rise(4) : null;
-		window.scrollY >= 6800 ? thisObj.scrollInSection_rise(5) : null;
-		window.scrollY >= 8500 ? thisObj.scrollInSection_rise(6) : null;
-		
-		window.scrollY >= 15000 ? thisObj.scrollInSection_rise(8) : null;
-		window.scrollY >= 16500 ? thisObj.scrollInSection_rise(9) : null;
-	});
+    const scrollY = window.scrollY;
+	console.log(scrollY);
+    
+    const scrollActions = [
+        { position: 1000, action: () => thisObj.scrollInSection_rise(2) },
+        { position: 2600, action: () => thisObj.scrollInSection_rise(3) },
+        { position: 5000, action: () => thisObj.scrollInSection_rise(4) },
+        { position: 6800, action: () => thisObj.scrollInSection_rise(5) },
+        { position: 8500, action: () => thisObj.scrollInSection_rise(6) },
+        { position: 9300, action: () => {
+            thisObj.scrollInSection_op(9300, 6, 1);
+            thisObj.scrollInSection_op(9500, 6, 2);
+            thisObj.scrollInSection_op(9700, 6, 3);
+        }},
+		{ position: 10000, action: () => thisObj.scrollInSection_panel() },
+		{ position: 11200, action: () => {
+            thisObj.scrollInSection_op(11200, 7, 1);
+            thisObj.scrollInSection_op(12100, 7, 2);
+            thisObj.scrollInSection_op(12500, 7, 3);
+			thisObj.scrollInSection_op(12800, 7, 0);
+			thisObj.scrollInSection_op(13300, 7, 4);
+			thisObj.scrollInSection_op(13700, 7, 5);
+			thisObj.scrollInSection_op(14400, 7, 6);
+        }},
+        { position: 15000, action: () => thisObj.scrollInSection_rise(8) },
+        { position: 16500, action: () => thisObj.scrollInSection_rise(9) },
+    ];
+
+    // 스크롤 위치에 따른 동작 수행
+    scrollActions.forEach(({ position, action }) => {
+        if (scrollY >= position) {
+            action();
+        }
+    });
+});
 
 };
 MainView.prototype.onInitDone = function()
@@ -44,6 +64,8 @@ MainView.prototype.onInitDone = function()
 	this.header_area.setStyle('position', 'fixed');
 	
 	this.hidden_view.addClass('visible');
+	
+	console.log(this.section7_left.element.style.transform);
 
 };
 
@@ -60,4 +82,32 @@ MainView.prototype.scrollInSection_rise = function(index)
 	this.findCompByGroup(`section${index}`).forEach((item) => {
 		item.addClass('on');
 	});
+};
+
+MainView.prototype.scrollInSection_op = function(scroll_pos, section_index, item_index) 
+{
+	if (section_index === 6) {
+		this.findCompByGroup(`section${section_index}_op${item_index}`).forEach((item)=>{
+			item.element.style.opacity = (window.scrollY - `${scroll_pos}`) * 0.005;
+		});
+	} else if (section_index === 7) {
+		this.findCompByGroup(`section${section_index}_op${item_index}`).forEach((item)=>{
+			
+			if (item_index === 0) {
+				var transY = (window.scrollY - 12800) * 0.005;
+				item.element.style.transform = `translateY(-${transY}%)`;
+				item.element.style.opacity = (window.scrollY - `${scroll_pos}`) * 0.002;
+			} else {
+				item.element.style.opacity = (window.scrollY - `${scroll_pos}`) * 0.002;
+			}
+		});
+	}
+	
+};
+
+MainView.prototype.scrollInSection_panel = function() 
+{
+	var transX = (window.scrollY - 10000) / 10;
+	this.section7_left.element.style.transform = `translateX(-${transX}%)`;
+	this.section7_right.element.style.transform = `translateX(${transX}%)`;
 };
